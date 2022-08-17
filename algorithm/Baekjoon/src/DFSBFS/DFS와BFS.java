531b4687a6104ffe6d76c8681fefa7c6acd88169
nnagman <ckdgh930314@naver.com>
2022-08-17 23:21:24 +0900

    백준 DFS BFS WIP

--- algorithm/Baekjoon/src/DFSBFS/DFS와BFS.java
+++ algorithm/Baekjoon/src/DFSBFS/DFS와BFS.java
@@ -3,49 +3,17 @@ package DFSBFS;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
-import java.util.stream.Collectors;
 
 public class DFS와BFS {
 
-    static class Node implements Comparable<Node> {
-
-        int now, to;
-
-        public Node(int now, int to) {
-            this.now = now;
-            this.to = to;
-        }
-
-
-        @Override
-        public int compareTo(Node o) {
-            if (this.now > o.now) {
-                return 1;
-            } else if (this.now < o.now) {
-                return -1;
-            } else {
-                if (this.to < o.to) {
-                    return -1;
-                } else {
-                    return 1;
-                }
-            }
-        }
-    }
-
     static int N, M, V;
-    // [n][0] -> DFS, n[1] -> BFS
-    static boolean[][] arr;
-
-    static int x, y;
-
-    static List<Node> list;
+    static int[][] arr;
+    static boolean[] visited;
+    static int a, b, temp;
+    static StringBuilder sb;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -58,33 +26,56 @@ public class DFS와BFS {
         // 시작 정점
         V = Integer.parseInt(st.nextToken());
 
-        arr = new boolean[N][2];
-
-        list = new ArrayList<>();
+        arr = new int[N + 1][N + 1];
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            x = Integer.parseInt(st.nextToken());
-            y = Integer.parseInt(st.nextToken());
-            list.add(new Node(x, y));
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            arr[a][b] = 1;
+            arr[b][a] = 1;
         }
 
-        Collections.sort(list);
+        visited = new boolean[N + 1];
+        dfs(V);
+
+        System.out.println();
+
+        visited = new boolean[N + 1];
+        bfs();
     }
 
-    static void bfs(int s) {
-        StringBuilder sb = new StringBuilder();
+    static void dfs(int n) {
+        System.out.print(n + " ");
+        visited[n] = true;
 
-        Queue<Node> q = new LinkedList<>(list.stream().filter(o -> o.now == V).toList());
-
-        sb.append(V).append(" ");
-        arr[V][1] = true;
-
-        while(!q.isEmpty()) {
-            Node node = q.poll();
-            if(!arr[node.to][1]) {
-                q.add(list.stream().filter(o -> o.now == V).findFirst().get());
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[n][i] == 1 && !visited[i]) {
+                visited[i] = true;
+                dfs(i);
             }
         }
+    }
+
+    static void bfs() {
+        Queue<Integer> q = new LinkedList<>();
+        sb = new StringBuilder();
+
+        q.add(V);
+        visited[V] = true;
+        sb.append(V).append(" ");
+
+        while (!q.isEmpty()) {
+            temp = q.poll();
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[temp][i] == 1 && !visited[i]) {
+                    q.add(i);
+                    visited[i] = true;
+                    sb.append(i).append(" ");
+                }
+            }
+        }
+
+        System.out.println(sb);
     }
 }
